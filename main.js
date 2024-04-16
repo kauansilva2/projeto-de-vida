152d63a0ef29044c1e5267b3df58c781ecf3838d
kauansilva2 <129184870+kauansilva2@users.noreply.github.com>
2024-04-16 08:36:17 -0300

    tryey

--- main.js
+++ main.js
@@ -72,3 +72,59 @@ const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("2024-10-05");
+
+contadores[0].textContent = tempoObjetivo1;
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+for (let i=-1 ; i<5; i++){
+    console.log(i);
+  }
+  const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-08-18T15:37:07");
+
+contadores[0].textContent = tempoObjetivo1;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
+
+let tempo = tempoObjetivo1.getTime();
+
+contadores[0].textContent = tempo;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
+
+let tempo = tempoObjetivo1.getDate();
+
+contadores[0].textContent = tempo;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
+
+let tempo = tempoObjetivo1.getMonth();
+
+contadores[0].textContent = tempo;
