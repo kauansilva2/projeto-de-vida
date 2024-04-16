a2af2a1b1ba7095864adaa8738ab3c967e11d75e
kauansilva2 <129184870+kauansilva2@users.noreply.github.com>
2024-04-16 08:38:25 -0300

    esgry

--- main.js
+++ main.js
@@ -128,3 +128,20 @@ const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
 let tempo = tempoObjetivo1.getMonth();
 
 contadores[0].textContent = tempo;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    .contador{
+        font-size: 20px;
+        text-align: center;
+    }
+        
