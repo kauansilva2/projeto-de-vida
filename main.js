7afd48dcd8291524b045d6bb9309e7e83e5bdd8f
kauansilva2 <129184870+kauansilva2@users.noreply.github.com>
2024-04-18 09:01:05 -0300

    bgh

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
-
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-
-        for(let j=0;j<botoes.length;j++){
-        botoes[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-    }
-}
-const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-botoes[j].classList.remove("ativo");
-textos[j].classList.remove("ativo");
+
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
@@ -28,107 +13,16 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
-const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem regressiva";
-const tempoObjetivo1 = new Date("2024-10-05");
 
-contadores[0].textContent = tempoObjetivo1;
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-botoes[j].classList.remove("ativo");
-textos[j].classList.remove("ativo");
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
 
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
-}
-const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem regressiva";
-const tempoObjetivo1 = new Date("2024-10-05");
-
-contadores[0].textContent = tempoObjetivo1;
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-botoes[j].classList.remove("ativo");
-textos[j].classList.remove("ativo");
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
-}
-const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem regressiva";
-const tempoObjetivo1 = new Date("2024-10-05");
-
-contadores[0].textContent = tempoObjetivo1;
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
-for (let i=-1 ; i<5; i++){
-    console.log(i);
-  }
-  const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07");
-
-contadores[0].textContent = tempoObjetivo1;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
-
-let tempo = tempoObjetivo1.getTime();
-
-contadores[0].textContent = tempo;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
-
-let tempo = tempoObjetivo1.getDate();
-
-contadores[0].textContent = tempo;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
-
-let tempo = tempoObjetivo1.getMonth();
-
-contadores[0].textContent = tempo;
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -136,12 +30,29 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    .contador{
-        font-size: 20px;
-        text-align: center;
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
     }
-        
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
